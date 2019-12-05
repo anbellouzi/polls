3f09277a6fd28788dc4b73063ec6516587323317
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-12-05 15:58:22 -0800

    depolying to heroko #1

---
+++ README.md
@@ -0,0 +1,32 @@
+Autor: Anas Bellouzi
+Date: 10/28/2019
+
+Project: Django Database
+
+Instruction:
+1. $ git clone https://github.com/anbellouzi/bew1.2_tutorials.git
+2. $ cd bew1.2_tutorials
+3. $ cd mysite
+4. $ python3 manage.py runserver
+5. On any browser visit: http://127.0.0.1:8000/admin/
+6. username: 'testing' , password: 'testing123'
+
+Features:
+  1. Add/Edit Question
+  2. Add/Edit Toppings
+  3. Add/Edit Pizza
+  4. Add/Edit User
+  5. Add/Edit Group
+
+Resources:
+  - Python
+  - Django
+
+Tutorials:
+  1. Writing your first Django app, part 1 -> https://docs.djangoproject.com/en/2.2/intro/tutorial01/
+  2. Writing your first Django app, part 2 -> https://docs.djangoproject.com/en/2.2/intro/tutorial02/
+  3. Writing your first Django app, part 3 -> https://docs.djangoproject.com/en/2.2/intro/tutorial03/
+
+Credit:
+Django Tips #7: https://wsvincent.com/class-function-based-views/
+Django Book:  https://djangobook.com/mdj2-models/
---
+++ polls/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Topping(models.Model):
+    toppings = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.toppings
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    toppings = models.ManyToManyField(Topping)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
